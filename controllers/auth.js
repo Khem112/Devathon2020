4f9bd5073f3ac99f3067b7a4ab448737e7838246
Khem112 <44099349+Khem112@users.noreply.github.com>
2020-10-03 09:36:55 +0530

    Add files via upload

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Cart = require("../models/cart");
 const { check, validationResult } = require("express-validator");
 var jwt = require("jsonwebtoken");
 var expressJwt = require("express-jwt");
@@ -93,3 +94,66 @@ exports.isAdmin = (req,res,next) => {
     }
     next();
 }
+
+exports.addCart = (req, res) => {
+    const errors = validationResult(req);
+  
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        error: errors.array()[0].msg
+      });
+    }
+    const cart = new Cart(req.body);
+      cart.save((err, cart) => {
+      if (err) {
+        return res.status(400).json({
+          err: "NOT able to save user in DB"
+        });
+      }
+      res.json({
+        total : cart.total,
+        key: cart.encry_key,
+
+      });
+    });
+  };
+
+  exports.getAllOrders = (req,res) => {
+    Cart.find().exec((err,carts)=>{
+      if(err || !carts){
+        return res.status(400).json({
+          "message" : "Currently no new order",
+        })
+      }
+      res.json(carts);
+    })
+  }
+
+exports.getOrderById = (req, res) => {
+    const errors = validationResult(req);
+    const { encry_key } = req.body;
+  
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        error: errors.array()[0].msg
+      });
+    }
+  
+    Cart.findOne({ encry_key }, (err, cart) => {
+      if (err || !cart) {
+        return res.status(400).json({
+          error: "Id does not exists"
+        });
+      }
+  
+      if (!cart.active==0) {
+        return res.status(401).json({
+          error: "Order Deactivated"
+        });
+      }
+  
+      //create token
+      const { eggs , veg1, non_veg1, total } = cart;
+      return res.json({ user: { eggs , veg1, non_veg1, total } });
+    });
+  };
